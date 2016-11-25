e8ba39fc793a48386e9710d926eac1c07e4cd90b
Ata Deniz Aydın <meagtan@users.noreply.github.com>
2016-11-26 01:17:10 +0300

    Cleanup

--- pq.c
+++ pq.c
@@ -1,23 +1,22 @@
 #include "maze.h"
 
-void siftdown(int[][], int, int);
-void siftup(int[][], int, int);
+void siftdown(int **, int, int);
+void siftup(int **, int, int);
 
-void push(int openset[][], int n, int x, int y, int dist, SearchState *st)
+void push(int **openset, int n, int x, int y, int dist, SearchState *st)
 {
     // if DFS, openset is a stack, so push and pop from end
     // if BFS, openset is a queue, so push at end and pop from start
     // else, openset is a priority queue with priority dist + heur(st, x, y)
-    
     openset[n][0] = x;
     openset[n][1] = y;
     openset[n][2] = dist + heur(st, x, y);
     
     if (st->type > BFS)
-        siftup(openset, 0, n + 1);
+        siftup(openset, 0, n);
 }
 
-void pop(int openset[][3], int n, int *x, int *y, SearchState *st)
+void pop(int **openset, int n, int *x, int *y, SearchState *st)
 {
     if (n <= 0) return;
     
@@ -35,16 +34,18 @@ void pop(int openset[][3], int n, int *x, int *y, SearchState *st)
         for (int i = 0; i < n - 1; ++i) {
             openset[i][0] = openset[i+1][0];
             openset[i][1] = openset[i+1][1];
+            openset[i][2] = openset[i+1][2];
         }
     } else if (n > 1) {
         openset[0][0] = openset[n-1][0];
         openset[0][1] = openset[n-1][1];
+        openset[0][2] = openset[n-1][2];
         siftdown(openset, 0, n - 1);
     }
 }
 
 // consecutive swaps could be optimized
-void siftdown(int openset[][], int start, int end)
+void siftdown(int **openset, int start, int end)
 {
     int child = 2 * start + 1, temp, i;
     while (child < end) {
@@ -66,7 +67,7 @@ void siftdown(int openset[][], int start, int end)
     }
 }
 
-void siftup(int openset[][], int start, int end)
+void siftup(int **openset, int start, int end)
 {
     int parent, i, temp;
     while (end > start) {
